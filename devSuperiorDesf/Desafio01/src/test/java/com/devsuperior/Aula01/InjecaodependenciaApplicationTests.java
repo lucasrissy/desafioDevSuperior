f0aa85c1fc3063f690b46d042cad17bf7d4ecd79
Lucas Rissi Andrade <lrissian@emeal.nttdata.com>
2023-12-07 11:33:01 +0000

    add framework spring

---
+++ devSuperiorDesf/Desafio01/src/test/java/com/devsuperior/Aula01/InjecaodependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Aula01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjecaodependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
